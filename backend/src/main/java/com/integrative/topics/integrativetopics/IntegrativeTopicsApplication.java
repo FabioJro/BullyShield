461199012b6deec827dc9dc7c7818b2ac95b33b6
Mateuzo <mateushcandido5@gmail.com>
2024-04-06 16:01:41 -0300

    Configuração inicial de projeto lado servidor

---
+++ backend/src/main/java/com/integrative/topics/integrativetopics/IntegrativeTopicsApplication.java
@@ -0,0 +1,13 @@
+package com.integrative.topics.integrativetopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrativeTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrativeTopicsApplication.class, args);
+	}
+
+}
