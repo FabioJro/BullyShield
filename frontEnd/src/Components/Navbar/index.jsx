f1e3008fe7fa0e98f51dcf4639a08eee67041088
Fabio Junior <frjcont@gmail.com>
2024-04-12 21:56:08 -0300

    Creating nav-bar

--- frontEnd/src/Components/Navbar/index.jsx
+++ frontEnd/src/Components/Navbar/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+
+import Style from "./navbar.module.css";
 
 const index = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
+  const menuNav = ["Inicio", "Sobre", "Mais"];
+  return (
+    <nav className={Style.navbar}>
+      <section className={Style.containerNavbar}>
+        <div className={Style.logo}>
+          <img className={Style.image} src="../../../public/logo.svg"></img>
+        </div>
+        <ul className={Style.containerItens}>
+          {menuNav.map((texto) => (
+            <li className={Style.li}>
+              <a className={Style.p}>{texto}</a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </nav>
+  );
+};
 
-export default index
+export default index;
