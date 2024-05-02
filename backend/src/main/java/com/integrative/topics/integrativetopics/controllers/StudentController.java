fd15a97973ab918decfd6df8238ca136f7c4c0cc
marcellojoaquim <marcellojoaquim1@hotmail.com>
2024-05-01 22:41:21 -0300

    controller student created

---
+++ backend/src/main/java/com/integrative/topics/integrativetopics/controllers/StudentController.java
@@ -0,0 +1,57 @@
+package com.integrative.topics.integrativetopics.controllers;
+
+import com.integrative.topics.integrativetopics.model.Student;
+import com.integrative.topics.integrativetopics.repository.StudentRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    private final StudentRepository students;
+
+    public StudentController(StudentRepository students){
+        this.students = students;
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id){
+        return students.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found."));
+    }
+
+    @GetMapping
+    public List<Student> findAll(Student filter){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filter, matcher);
+        return students.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student save(@RequestBody Student student){
+        return students.save(student);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        students.findById(id)
+                .map(student -> {students.delete(student);
+                return student;
+                }).orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found."));
+    }
+
+    
+}
