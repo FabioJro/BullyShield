ae82582a43799073ace927037e822f89fbb69001
marcellojoaquim <marcellojoaquim1@hotmail.com>
2024-05-08 23:17:36 -0300

    atualize branch from remote

--- backend/src/main/java/com/integrative/topics/integrativetopics/controllers/ProfessorController.java
+++ backend/src/main/java/com/integrative/topics/integrativetopics/controllers/ProfessorController.java
@@ -1,11 +1,22 @@
 package com.integrative.topics.integrativetopics.controllers;
 
+import com.integrative.topics.integrativetopics.dtos.ProfessorDTO;
+import com.integrative.topics.integrativetopics.model.Professor;
+import com.integrative.topics.integrativetopics.repository.ProfessorRepository;
+import com.integrative.topics.integrativetopics.services.ProfessorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/professor")
 public class ProfessorController {
+    private ProfessorService professorService;
+
+
 
 
 }
