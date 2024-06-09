041f0f8e3b9908c37418457de1c1beb96f2bd90a
Vanildo <jvcnf21@gmail.com>
2024-06-09 13:31:05 -0300

    modal menssagem

--- frontEnd/src/Components/CardProfessores/Components/ModalAluno/index.jsx
+++ frontEnd/src/Components/CardProfessores/Components/ModalAluno/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import Style from "./ModalAluno.module.css";
 const index = ({id, onClose}) => {
   const historico = [
@@ -140,64 +140,74 @@ const index = ({id, onClose}) => {
       ],
     },
   ];
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+
+  const handleSendMessage = () => {
+    setIsSending(true);
+    setTimeout(() => {
+      setIsSending(false);
+      setIsSent(true);
+      setTimeout(() => {
+        setIsSent(false);
+      }, 2000);
+    }, 2000);
+  };
+
   return (
     <section className={Style.bodyPage}>
       <button className={Style.fecharModal} onClick={onClose}>x</button>
       <section className={Style.bodyHistorico}>
-        {historico.map((card, key) => {
-          return (
-            <div className={Style.modal} key={key}>
-              <div className={Style.modalHistorico}>
-                <h2 className={Style.historico}>Historico do Aluno</h2>
-              </div>
-              <div className={Style.infoAluno}>
-                <div className={Style.modalNome}>
-                  <div className={Style.nome}>
-                    <p>
-                        {id.nome}
-                    </p>
-                  </div>
-                  <div div className={Style.matricula}>
-                    <p>Matricula : {id.matricula}</p>
-                  </div>
+        {historico.map((card, key) => (
+          <div className={Style.modal} key={key}>
+            <div className={Style.modalHistorico}>
+              <h2 className={Style.historico}>Historico do Aluno</h2>
+            </div>
+            <div className={Style.infoAluno}>
+              <div className={Style.modalNome}>
+                <div className={Style.nome}>
+                  <p>{card.nome}</p>
                 </div>
-              </div>
-              <div className={Style.info}>
-                <div className={Style.modalInfo}>
-                  <div div className={Style.matricula}>
-                    <p>Disciplina</p>
-                  </div>
-                  <div div className={Style.media}>
-                    <p>Nota Media</p>
-                  </div>
-                  <div div className={Style.frequencia}>
-                    <p>Frequencia</p>
-                  </div>
+                <div className={Style.matricula}>
+                  <p>Matricula : {card.matricula}</p>
                 </div>
-              </div>
-              <div className={Style.modalDisciplina}>
-                <p>
-                  {id && id.disciplinas.map((disciplina, key) => {
-                    return (
-                      <div className={Style.modalAluno}>
-                        <div div className={Style.modalNomeDisciplina}>
-                          <p>{disciplina.nome}</p>
-                        </div>
-                        <div div className={Style.modalmediaNotaDisciplina}>
-                          <p>{disciplina.media}</p>
-                        </div>
-                        <div className={Style.modalfrequenciaMediaDisciplina}>
-                          <p>{disciplina.frequencia}</p>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </p>
               </div>
             </div>
-          );
-        })}
+            <div className={Style.info}>
+              <div className={Style.modalInfo}>
+                <div className={Style.matricula}>
+                  <p>Disciplina</p>
+                </div>
+                <div className={Style.media}>
+                  <p>Nota Media</p>
+                </div>
+                <div className={Style.frequencia}>
+                  <p>Frequencia</p>
+                </div>
+              </div>
+            </div>
+            <div className={Style.modalDisciplina}>
+              {card.disciplinas.map((disciplina, key) => (
+                <div className={Style.modalAluno} key={key}>
+                  <div className={Style.modalNomeDisciplina}>
+                    <p>{disciplina.nome}</p>
+                  </div>
+                  <div className={Style.modalmediaNotaDisciplina}>
+                    <p>{disciplina.media}</p>
+                  </div>
+                  <div className={Style.modalfrequenciaMediaDisciplina}>
+                    <p>{disciplina.frequencia}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </section>
+        <button onClick={handleSendMessage} className={Style.sendMessageButton}>
+          {isSending ? 'Enviando mensagem...' : 'Enviar mensagem'}
+        </button>
+        {isSent && <div className={Style.popupMessage}>Mensagem enviada!</div>}
     </section>
   );
 };
